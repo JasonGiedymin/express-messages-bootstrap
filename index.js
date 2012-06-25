03987c3b8dbac4466d2c255bdf7f388b42de41ee
Nick Baugh <niftylettuce@gmail.com>
2012-06-25 17:50:12 -0400

    Added fade and in classes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ module.exports = function(req, res) {
       if (msgs != null) {
         for (j = 0, _ref = msgs.length; 0 <= _ref ? j < _ref : j > _ref; 0 <= _ref ? j++ : j--) {
           msg = msgs[j];
-          buf.push("<div class=\"alert alert-" + type + "\">");
+          buf.push("<div class=\"alert alert-" + type + " fade in\">");
           buf.push("<a class=\"close\" data-dismiss=\"alert\">&times;</a>");
           buf.push(msg);
           buf.push("</div>");
